20951009640d909ae3806ddfb852972453be8d83
loint <loint@penlook.com>
2015-01-28 21:38:40 +0700

    Remove TCP handler and using custom handler

--- service.go
+++ service.go
@@ -2,11 +2,8 @@ package daemon
 
 import (
 	"fmt"
-	"log"
-	"net"
 	"os"
 	"os/signal"
-	"strconv"
 	"syscall"
 )
 
@@ -41,64 +38,11 @@ func (daemon *ServiceDaemon) Manage(service Service) (string, error) {
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt, os.Kill, syscall.SIGTERM)
 
-	//process := service.GetProcess()
-	//process()
-
-	// Set up listener for defined host and port
-	listener, err := net.Listen("tcp", ":"+strconv.Itoa(80))
-
-	if err != nil {
-		return "Possibly was a problem with the port binding", err
-	}
-
-	// set up channel on which to send accepted connections
-	listen := make(chan net.Conn, 100)
-	go acceptConnection(listener, listen)
-
-	// loop work cycle with accept connections or interrupt
-	// by system signal
-	for {
-		select {
-		case conn := <-listen:
-			go handleClient(conn)
-		case killSignal := <-interrupt:
-			log.Println("Got signal:", killSignal)
-			log.Println("Stoping listening on ", listener.Addr())
-			listener.Close()
-			if killSignal == os.Interrupt {
-				return "Daemon was interruped by system signal", nil
-			}
-			return "Daemon was killed", nil
-		}
-	}
+	process := service.GetProcess()
+	process()
 
 	// never happen, but need to complete code
 	return usage, nil
-}
-
-// Accept a client connection and collect it in a channel
-func acceptConnection(listener net.Listener, listen chan<- net.Conn) {
-	log.Println("ACCEPT CONNECTION")
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			continue
-		}
-		listen <- conn
-	}
-}
-
-func handleClient(client net.Conn) {
-	log.Println("HANDLE CLIENT")
-	for {
-		buf := make([]byte, 4096)
-		numbytes, err := client.Read(buf)
-		if numbytes == 0 || err != nil {
-			return
-		}
-		log.Println(string(buf))
-		client.Write(buf)
-	}
 }
 
 // Services
