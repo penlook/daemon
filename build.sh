e72fb099cfb6456fb5e8f4755d45facf65d8c7a1
tinntt <ntttin132@gmail.com>
2015-03-01 14:25:02 +0700

    make daemon directory

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 CURRENT=`pwd`
 USER=`whoami`
 ROOT="/home/$USER/src/github.com"
+sudo mkdir -p $ROOT/penlook/service/component/daemon
 cp -rf ./* $ROOT/penlook/service/component/daemon
 cd $ROOT/penlook/service/component/daemon
 go build
