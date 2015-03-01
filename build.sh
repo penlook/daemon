946c4446d98620f23dd4551c4fdbf9710adda029
tinntt <ntttin132@gmail.com>
2015-03-01 14:02:12 +0700

    fixed wrong directory

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 CURRENT=`pwd`
 USER=`whoami`
 ROOT="/home/$USER/src/github.com"
-cp -rf ./* $ROOT/penlook/daemon
-cd $ROOT/penlook/daemon
+cp -rf ./* $ROOT/penlook/service/component/daemon
+cd $ROOT/penlook/service/component/daemon
 go build
 cd $CURRENT
