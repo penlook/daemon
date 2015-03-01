e33e141950d3092042ff9bee6552839f85515de5
tinntt <ntttin132@gmail.com>
2015-03-01 14:33:40 +0700

    fix permission for build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ CURRENT=`pwd`
 USER=`whoami`
 ROOT="/home/$USER/src/github.com"
 sudo mkdir -p $ROOT/penlook/service/component/daemon
-cp -rf ./* $ROOT/penlook/service/component/daemon
+sudo cp -rf ./* $ROOT/penlook/service/component/daemon
 cd $ROOT/penlook/service/component/daemon
 go build
 cd $CURRENT
